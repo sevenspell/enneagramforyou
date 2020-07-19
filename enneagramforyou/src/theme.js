71e628b70b0ea215f49965c6addcc0c7d03f029c
Seven <sevenspell@gmail.com>
2020-07-20 00:26:14 +1000

    removed indigo in theme

--- enneagramforyou/src/theme.js
+++ enneagramforyou/src/theme.js
@@ -1,6 +1,5 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 import purple from '@material-ui/core/colors/purple';
-import indigo from '@material-ui/core/colors/indigo';
 import amber from '@material-ui/core/colors/amber';
 import lime from '@material-ui/core/colors/lime';
 
