ad2840c3d754680cf233ef5a458f4162c15a3701
Seven <sevenspell@gmail.com>
2020-07-20 00:22:35 +1000

    edit on enneagram

--- enneagramforyou/src/pages/Enneagram/Enneagram.js
+++ enneagramforyou/src/pages/Enneagram/Enneagram.js
@@ -2,9 +2,7 @@
 import React, { useState, useContext, useEffect } from 'react';
 import Header from "../../components/Header/Header"
 import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
