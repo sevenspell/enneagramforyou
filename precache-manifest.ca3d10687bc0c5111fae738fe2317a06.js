e76d05939ec779bb1ad4bf71913d18c9bd647702
Seven <sevenspell@gmail.com>
2020-07-21 01:25:40 +1000

    Updates

---
+++ precache-manifest.ca3d10687bc0c5111fae738fe2317a06.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "771033e7a85e0c25b0180c02be098a18",
+    "url": "/sevenspell/enneagramforyou/index.html"
+  },
+  {
+    "revision": "b2ddb46f1bdea8abc1bf",
+    "url": "/sevenspell/enneagramforyou/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3456815eb958d7a9eb4c",
+    "url": "/sevenspell/enneagramforyou/static/js/2.79166dc5.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/sevenspell/enneagramforyou/static/js/2.79166dc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2ddb46f1bdea8abc1bf",
+    "url": "/sevenspell/enneagramforyou/static/js/main.798c2f87.chunk.js"
+  },
+  {
+    "revision": "f5b21ba89d9ca8d910f0",
+    "url": "/sevenspell/enneagramforyou/static/js/runtime-main.79d737c7.js"
+  },
+  {
+    "revision": "68ba501cab184fee8ae331b74708eabf",
+    "url": "/sevenspell/enneagramforyou/static/media/egimage.68ba501c.png"
+  }
+]);
